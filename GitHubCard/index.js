93f8d14959d2090abdfc1446e29bddf372deb401
Sara Cearc <cearc-sara@lambdastudents.com>
2020-08-06 16:35:34 -0400

    added variables to the function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,17 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
+    https://api.github.com/users/cearc-sara
 */
+axios.get('https://api.github.com/users/cearc-sara')
+  .then(response => {
 
+  })
+  .catch(error => {
+    debugger
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,7 +57,27 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker(object){
+  const card = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const gitHubAddress = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
 
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  username.classList.add('username')
+
+  image.src = object.avatar_url
+
+}
 /*
   List of LS Instructors Github username's:
     tetondan
